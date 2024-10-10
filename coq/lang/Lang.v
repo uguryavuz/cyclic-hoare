d8531eb5d857a0f22f7f63d3bd736ea1a07b2f53
Ugur Yavuz <35351115+uguryavuz@users.noreply.github.com>
2024-10-10 18:34:37 -0400

    Asserted assertions

--- coq/lang/Lang.v
+++ coq/lang/Lang.v
@@ -63,6 +63,7 @@ Qed.
 
 End Memory.
 Notation varid := string.
+Implicit Type m : mem.
 
 
 Section Language.
@@ -213,6 +214,392 @@ Ltac empty_inhab_false :=
   end.
 
 
+
+Section Binding.
+
+Notation ivarid := string.
+Inductive ivar : Type :=
+| IVar (i : ivarid)
+.
+
+Definition binding := ivar -> int.
+Implicit Type I : binding.
+
+
+Definition iupd I i n : binding :=
+  fun i' => If i = i' then n else I i'.
+
+
+Lemma iupd_shadow1 I i n n' :
+  iupd (iupd I i n) i n' = iupd I i n'.
+Proof using.
+  unfolds iupd. extensionality x'.
+  case_if~.
+Qed.
+
+Lemma iupd_shadow2 I i j k n n' :
+  iupd (iupd (iupd I i n) j k) i n' = iupd (iupd I j k) i n'.
+Proof using.
+  unfold iupd. extensionality x'.
+  case_if~.
+Qed.
+
+Lemma iupd_swap_diff I i i' n n' :
+  i <> i' ->
+  iupd (iupd I i n) i' n' = iupd (iupd I i' n') i n.
+Proof using.
+  intros.
+  extensionality i''.
+  unfolds. case_if~.
+  case_if~.
+Qed.
+
+Lemma iupd_read1 I i n :
+  (iupd I i n) i = n.
+Proof using.
+  unfolds. case_if~.
+Qed.
+
+Lemma iupd_read2 I i i' n n' :
+  i <> i' ->
+  iupd (iupd I i n) i' n' i = n.
+Proof using.
+  intros.
+  unfolds. case_if~. case_if~.
+Qed.
+
+End Binding.
+Notation ivarid := string.
+Implicit Type I : binding.
+
+Section Assertions.
+
+Inductive aexpv :=
+| AvVal (n:int)
+| AvVar (x:varid)
+| AvNeg (a:aexpv)
+| AvAdd (a1 a2:aexpv)
+| AvSub (a1 a2:aexpv)
+| AvMul (a1 a2:aexpv)
+| AvIVar (i:ivar)
+.
+
+Inductive assrt :=
+| AssrtVal (b:bool)
+| AssrtNot (b:assrt)
+| AssrtAnd (b1 b2:assrt)
+| AssrtOr  (b1 b2:assrt)
+| AssrtImp (b1 b2:assrt)
+| AssrtEqB (b1 b2:assrt)
+| AssrtEqA (a1 a2:aexpv)
+| AssrtLt  (a1 a2:aexpv)
+| AssrtLeq (a1 a2:aexpv)
+| AssrtForall (i:ivar) (b:assrt)
+| AssrtExists (i:ivar) (b:assrt)
+.
+
+Fixpoint aveval m I a : int :=
+match a with
+| AvVal n => n
+| AvVar x => m x
+| AvIVar i => I i
+| AvNeg a => -(aveval m I a)
+| AvAdd a1 a2 => (aveval m I a1) + (aveval m I a2)
+| AvSub a1 a2 => (aveval m I a1) - (aveval m I a2)
+| AvMul a1 a2 => (aveval m I a1) * (aveval m I a2)
+end.
+
+Implicit Type P Q : assrt.
+
+Fixpoint sat m I P : Prop :=
+  match P with
+  | AssrtVal b => b
+  | AssrtNot P => ~ sat m I P
+  | AssrtAnd P Q => sat m I P /\ sat m I Q
+  | AssrtOr P Q => sat m I P \/ sat m I Q
+  | AssrtImp P Q => sat m I P -> sat m I Q
+  | AssrtEqB P Q => sat m I P = sat m I Q
+  | AssrtEqA a1 a2 => aveval m I a1 = aveval m I a2
+  | AssrtLt a1 a2 => aveval m I a1 < aveval m I a2
+  | AssrtLeq a1 a2 => aveval m I a1 <= aveval m I a2
+  | AssrtForall i P => forall n, sat m (iupd I i n) P
+  | AssrtExists i P => exists n, sat m (iupd I i n) P
+  end.
+
+Notation "m ',' I '|=' P" := (sat m I P) (at level 50).
+
+Fixpoint aexp_to_aexpv a : aexpv :=
+  match a with
+  | AVal n => AvVal n
+  | AVar x => AvVar x
+  | ANeg a => AvNeg (aexp_to_aexpv a)
+  | AAdd a1 a2 => AvAdd (aexp_to_aexpv a1) (aexp_to_aexpv a2)
+  | ASub a1 a2 => AvSub (aexp_to_aexpv a1) (aexp_to_aexpv a2)
+  | AMul a1 a2 => AvMul (aexp_to_aexpv a1) (aexp_to_aexpv a2)
+  end.
+
+Lemma aexp_aexpv_equiv m I a :
+  aveval m I (aexp_to_aexpv a) = aeval m a.
+Proof using.
+  induction a; simpls; f_equal~.
+Qed.
+
+Fixpoint bexp_to_assrt b : assrt :=
+  match b with
+  | BVal b => AssrtVal b
+  | BNot b => AssrtNot (bexp_to_assrt b)
+  | BAnd b1 b2 => AssrtAnd (bexp_to_assrt b1) (bexp_to_assrt b2)
+  | BOr  b1 b2 => AssrtOr (bexp_to_assrt b1) (bexp_to_assrt b2)
+  | BImp b1 b2 => AssrtImp (bexp_to_assrt b1) (bexp_to_assrt b2)
+  | BEqB b1 b2 => AssrtEqB (bexp_to_assrt b1) (bexp_to_assrt b2)
+  | BEqA a1 a2 => AssrtEqA (aexp_to_aexpv a1) (aexp_to_aexpv a2)
+  | BLt  a1 a2 => AssrtLt (aexp_to_aexpv a1) (aexp_to_aexpv a2)
+  | BLeq a1 a2 => AssrtLeq (aexp_to_aexpv a1) (aexp_to_aexpv a2)
+  end.
+
+Lemma bexp_assrt_equiv m I b :
+  sat m I (bexp_to_assrt b) <-> beval m b.
+Proof using.
+  induction b; simpls.
+  - reflexivity.
+  - split; intros. contra H. apply IHb. 
+    destruct (beval m b); simpls; auto.
+    intro. apply IHb in H0. rewrite H0 in H. auto.
+  - rewrite IHb1, IHb2.
+    rewrite istrue_and_eq.
+    easy.
+  - rewrite IHb1, IHb2.
+    rewrite istrue_or_eq.
+    easy.
+  - rewrite IHb1, IHb2.
+    destruct (beval m b1), (beval m b2); simpls.
+    + easy.
+    + split. intro. auto. auto.
+    + split. intro. auto. auto.
+    + split. intro. auto. auto.
+  - split.
+    + intro. destruct (beval m b1), (beval m b2); simpls; auto;
+      rewrite H in IHb1; rewrite IHb1 in IHb2; apply IHb2; auto.
+    + intro. 
+      destruct (beval m b1), (beval m b2); simpls; try easy.
+      * rewrite <- IHb1 in IHb2. apply prop_ext. easy.
+      * rewrite <- IHb1 in IHb2. apply prop_ext. easy.
+  - do 2 rewrite aexp_aexpv_equiv.
+    rewrite <- Z.eqb_eq. easy.
+  - do 2 rewrite aexp_aexpv_equiv.
+    rewrite <- bool_eq_true_eq.
+    split; intros.
+    * apply Z.ltb_lt.
+      math.
+    * apply Z.ltb_lt in H.
+      math.
+  - do 2 rewrite aexp_aexpv_equiv.
+    rewrite <- bool_eq_true_eq.
+    split; intros.
+    * apply Z.leb_le.
+      math.
+    * apply Z.leb_le in H.
+      math.
+Qed.
+
+Fixpoint has_ivars (a : aexpv) : Prop :=
+  match a with
+  | AvVal _ => False
+  | AvVar _ => False
+  | AvNeg a => has_ivars a
+  | AvAdd a1 a2 => has_ivars a1 \/ has_ivars a2
+  | AvSub a1 a2 => has_ivars a1 \/ has_ivars a2
+  | AvMul a1 a2 => has_ivars a1 \/ has_ivars a2
+  | AvIVar _ => True
+  end.
+
+Lemma aexp_no_ivars (a : aexp) :
+  ~ has_ivars (aexp_to_aexpv a).
+Proof.
+  induction a; simpls; intro; auto; destruct~ H.
+Qed.
+
+Definition aexpv_subst (x : varid) (a' : aexpv) : aexpv -> aexpv :=
+  fix rec (a : aexpv) :=
+    match a with
+    | AvVal n => AvVal n
+    | AvVar x' => If x = x' then a' else AvVar x'
+    | AvNeg a => AvNeg (rec a)
+    | AvAdd a1 a2 => AvAdd (rec a1) (rec a2)
+    | AvSub a1 a2 => AvSub (rec a1) (rec a2)
+    | AvMul a1 a2 => AvMul (rec a1) (rec a2)
+    | AvIVar i => AvIVar i
+    end.
+
+Definition aexpv_isubst (i : ivar) (a' : aexpv) : aexpv -> aexpv :=
+  fix rec (a : aexpv) :=
+    match a with
+    | AvVal n => AvVal n
+    | AvVar x => AvVar x
+    | AvNeg a => AvNeg (rec a)
+    | AvAdd a1 a2 => AvAdd (rec a1) (rec a2)
+    | AvSub a1 a2 => AvSub (rec a1) (rec a2)
+    | AvMul a1 a2 => AvMul (rec a1) (rec a2)
+    | AvIVar i' => If i = i' then a' else AvIVar i'
+    end. 
+
+Definition assrt_subst (x : varid) (a : aexpv) (H : ~ has_ivars a) : assrt -> assrt :=
+  fix rec (P : assrt) :=
+    match P with
+    | AssrtVal b => AssrtVal b
+    | AssrtNot P => AssrtNot (rec P)
+    | AssrtAnd P Q => AssrtAnd (rec P) (rec Q)
+    | AssrtOr P Q => AssrtOr (rec P) (rec Q)
+    | AssrtImp P Q => AssrtImp (rec P) (rec Q)
+    | AssrtEqB P Q => AssrtEqB (rec P) (rec Q)
+    | AssrtEqA a1 a2 => AssrtEqA (aexpv_subst x a a1) (aexpv_subst x a a2)
+    | AssrtLt a1 a2 => AssrtLt (aexpv_subst x a a1) (aexpv_subst x a a2)
+    | AssrtLeq a1 a2 => AssrtLeq (aexpv_subst x a a1) (aexpv_subst x a a2)
+    | AssrtForall i P => AssrtForall i (rec P)
+    | AssrtExists i P => AssrtExists i (rec P)
+    end.
+
+Definition assrt_isubst (i : ivar) (a : aexpv) (H : ~ has_ivars a) : assrt -> assrt :=
+  fix rec (P : assrt) :=
+    match P with
+    | AssrtVal b => AssrtVal b
+    | AssrtNot P => AssrtNot (rec P)
+    | AssrtAnd P Q => AssrtAnd (rec P) (rec Q)
+    | AssrtOr P Q => AssrtOr (rec P) (rec Q)
+    | AssrtImp P Q => AssrtImp (rec P) (rec Q)
+    | AssrtEqB P Q => AssrtEqB (rec P) (rec Q)
+    | AssrtEqA a1 a2 => AssrtEqA (aexpv_isubst i a a1) (aexpv_isubst i a a2)
+    | AssrtLt a1 a2 => AssrtLt (aexpv_isubst i a a1) (aexpv_isubst i a a2)
+    | AssrtLeq a1 a2 => AssrtLeq (aexpv_isubst i a a1) (aexpv_isubst i a a2)
+    | AssrtForall i' P => AssrtForall i' (If i = i' then P else rec P)
+    | AssrtExists i' P => AssrtExists i' (If i = i' then P else rec P)
+    end.
+
+Lemma aexpv_subst_equiv m I x a n : 
+  aveval m I (aexpv_subst x (AvVal n) a) = aveval (upd m x n) I a.
+Proof using.
+  induction a; simpls; auto; try math.
+  + case_if; simpls.
+    * subst. rewrite upd_read1. trivial.
+    * unfold upd. case_if. trivial.
+Qed.
+
+Lemma aexpv_isubst_equiv m I i a n : 
+  aveval m I (aexpv_isubst i (AvVal n) a) = aveval m (iupd I i n) a.
+Proof using.
+  induction a; simpls; auto; try math.
+  + case_if; simpls.
+    * subst. rewrite iupd_read1. trivial.
+    * unfold iupd. case_if. trivial.
+Qed.
+
+Lemma assrt_subst_equiv m I x n P H :
+  sat m I (assrt_subst x (AvVal n) H P) <-> sat (upd m x n) I P.
+Proof using.
+  generalize dependent n. 
+  generalize dependent I. 
+  induction P; simpls; auto; try easy.
+  - intros. rewrite IHP. easy.
+  - intros. rewrite IHP1, IHP2. easy.
+  - intros. rewrite IHP1, IHP2. easy.
+  - intros. rewrite IHP1, IHP2. easy.
+  - intros. split. 
+    + intros. 
+      specializes IHP1 n. 
+      specialize IHP1 with H. 
+      rewrite H0 in IHP1. 
+      specializes IHP2 n.
+      specialize IHP2 with H.
+      rewrite IHP1 in IHP2. apply prop_ext. auto.
+    + intros. 
+      specializes IHP1 n. 
+      specialize IHP1 with H. 
+      rewrite H0 in IHP1. 
+      specializes IHP2 n.
+      specialize IHP2 with H.
+      rewrite <- IHP1 in IHP2. apply prop_ext. symmetry. auto.
+  - intros. do 2 rewrite aexpv_subst_equiv. easy.
+  - intros. do 2 rewrite aexpv_subst_equiv. easy.
+  - intros. do 2 rewrite aexpv_subst_equiv. easy.
+  - split. 
+    + intros. 
+      eapply IHP.
+      apply H0. 
+    + intros.
+      eapply IHP.
+      apply H0.
+  - intros. split.
+    + intros (?&?).
+      exists x0.
+      eapply IHP.
+      apply H0.
+    + intros (?&?).
+      exists x0.
+      eapply IHP.
+      apply H0. 
+Qed.
+
+Lemma assrt_isubst_equiv m I i n P H :
+  sat m I (assrt_isubst i (AvVal n) H P) <-> sat m (iupd I i n) P.
+Proof using.
+  generalize dependent n. 
+  generalize dependent I. 
+  induction P; simpls; auto; try easy.
+  - intros. rewrite IHP. easy.
+  - intros. rewrite IHP1, IHP2. easy.
+  - intros. rewrite IHP1, IHP2. easy.
+  - intros. rewrite IHP1, IHP2. easy.
+  - intros. split. 
+    + intros. 
+      specializes IHP1 n. 
+      specialize IHP1 with H. 
+      rewrite H0 in IHP1. 
+      specializes IHP2 n.
+      specialize IHP2 with H.
+      rewrite IHP1 in IHP2. apply prop_ext. auto.
+    + intros. 
+      specializes IHP1 n. 
+      specialize IHP1 with H. 
+      rewrite H0 in IHP1. 
+      specializes IHP2 n.
+      specialize IHP2 with H.
+      rewrite <- IHP1 in IHP2. apply prop_ext. symmetry. auto.
+  - intros. do 2 rewrite aexpv_isubst_equiv. easy.
+  - intros. do 2 rewrite aexpv_isubst_equiv. easy.
+  - intros. do 2 rewrite aexpv_isubst_equiv. easy.
+  - intros. split.
+    + intros. case_if.
+      * subst.
+        rewrite iupd_shadow1.
+        apply H0.
+      * rewrite iupd_swap_diff; auto.
+        eapply IHP.
+        apply H0.
+    + intros. case_if.
+      * subst.
+        erewrite <- iupd_shadow1.
+        apply H0.
+      * eapply IHP.
+        rewrite iupd_swap_diff; auto.
+  - intros. split.
+    + intros (?&?). case_if.
+      * subst.
+        exists x.
+        now rewrite iupd_shadow1.
+      * exists x.
+        rewrite iupd_swap_diff; auto.
+        eapply IHP.
+        apply H0.
+    + intros (?&?). case_if.
+      * subst. 
+        exists x. 
+        now rewrite iupd_shadow1 in H0.
+      * exists x.
+        eapply IHP.
+        rewrite iupd_swap_diff; auto.
+Qed.
+
+End Assertions.
 
 Section Interp.
 
