8ad0a11a6095abc87db4842af250b0cc33c854e2
jaredpincus <jaredpincus@gmail.com>
2025-02-22 16:26:42 -0500

    tweak

--- coq/lang/UnaryHLGraphs.v
+++ coq/lang/UnaryHLGraphs.v
@@ -244,6 +244,7 @@ Section GraphFacts.
 
 Variable rg : rule_graph.
 Variable lift_valid : graph_lift_valid rg.
+Variable acyclic : ~ cyclic rg.
 
 Definition form_1 P Q :=
   |- (CWhile true CSkip) : P => Q.
@@ -361,8 +362,7 @@ Lemma form_3_skip nd P Q :
 Proof.
   intros Conc HP.
   pose proof rg_wf nd. rewrite Conc in H.
-  
-
+  (* TODO will rely on acyclicity *)
 Admitted.
 
 Lemma form_2_seq nd P Q :
@@ -386,7 +386,6 @@ Proof.
   2: { right. constructor~. }
   applys~ form_3_skip r P.
 Qed.
-  
 
 Definition has_form nd P Q :=
   let conc := rg_conc nd in
@@ -454,8 +453,8 @@ Proof.
   intro. specializes H (|- (CWhile true CSkip) : true => false).
   specializes H infinite_loop_valid.
   destruct H as (rg&(Lft&nd&Conc)&Snd&Acyclic).
+  pose proof @path_grow _ Lft Acyclic nd true false.
   contradict Acyclic. apply cyclic_cardinal.
-  pose proof @path_grow _ Lft nd true false.
   specializes H (NodeSet.cardinal (rg_nodes rg)); try easy.
   { unfolds. left. now rewrite Conc. }
   destruct H as (_&_&_&p&_&_&_&_&L).
@@ -463,213 +462,3 @@ Proof.
 Qed.
 
 End AcyclicIncompleteness.
-
-
-
-
-(*Section GraphFacts.
-
-Variable rg : rule_graph.
-Variable lift_valid : graph_lift_valid rg.
-
-Definition form_1 P Q :=
-  (|- (CWhile true CSkip) : P => Q).
-
-Definition form_2 P Q :=
-  (|- (CWhile true CSkip) : (P /\ true) => (Q /\ true)).
-
-Definition form_3 P Q :=
-  (|- (CSeq CSkip (CWhile true CSkip)) : P => Q).
-
-Implicit Type nd : rg_node rg.
-
-Lemma form_1_rules nd P Q :
-  rg_conc nd = form_1 P Q ->
-  (|= P)%V ->
-  rg_rule nd = Rule RGP.HL_CSQ \/ 
-  rg_rule nd = Rule RGP.HL_WhileTrue.
-Proof.
-  intros Conc TautP.
-  pose proof @rg_wf rg nd.
-  destruct (rg_rule nd) eqn:R.
-  2: { 
-    destruct H. unfolds in H. rewrite Conc in H.
-    now unfolds form_1.
-  }
-  rewrite Conc in H. unfolds form_1.
-  destruct~ r; inverts H.
-  (* Show that HL_False cannot be applied *)
-  apply valid_and in TautP as (?&?).
-  unfolds in H1.
-  specialize H1 with (fun _ => 0) (fun _ => 0).
-  simpls. now contradict H1.
-Qed.
-
-Lemma form_2_rules nd P Q :
-  rg_conc nd = form_2 P Q ->
-  (|= P)%V ->
-  rg_rule nd = Rule RGP.HL_CSQ \/ 
-  rg_rule nd = Rule RGP.HL_WhileTrue.
-Proof.
-  pose proof @form_1_rules nd (P /\ true)%A (Q /\ true)%A.
-  intros. applys~ H.
-  apply valid_and. splits~. unfolds. intros. now simpls.
-Qed.
-
-Lemma form_3_rules nd P Q :
-  rg_conc nd = form_3 P Q ->
-  rg_rule nd = Rule RGP.HL_CSQ \/ 
-  rg_rule nd = Rule RGP.HL_Seq.
-Proof.
-  intros Conc.
-  pose proof @rg_wf rg nd.
-  destruct (rg_rule nd) eqn:R.
-  2: { 
-    destruct H. unfolds in H. now rewrite Conc in H.
-  }
-  rewrite Conc in H. unfolds form_3.
-  destruct~ r; inverts H.
-Qed.
-
-
-Lemma strong_csq nd P Q c :
-  rg_conc nd = |- c : P => Q ->
-  rg_rule nd = Rule RGP.HL_CSQ ->
-  (|= P)%V ->
-  (|= ~Q)%V ->
-  exists nd' P' Q',
-  (|= P')%V /\ (|= ~Q')%V /\
-  rg_conc nd' = |- c : P' => Q' /\
-  List.In nd' (rg_prems nd).
-Proof.
-  intros Conc Rule TautP TautQ.
-  pose proof rg_wf nd. rewrite Rule in H.
-  inverts H. rewrite Conc in H1.
-  unfold form_1 in H1. inverts H1. sort.
-  (* Break down prems list into 3 items *)
-  destruct (rg_prems nd). discriminate.
-  simpls. destruct l. discriminate. simpls.
-  destruct l. discriminate.
-  simpls. destruct l. 2: discriminate.
-  simpls. inverts H0.
-  exists r0 P' Q'. splits~.
-  - specializes lift_valid r.
-    rewrite <- H1 in lift_valid.
-    destruct (rg_rule r) eqn:E.
-    pose proof rg_wf r. rewrite E in H.
-    rewrite <- H1 in H. inverts H.
-    unfolds in lift_valid.
-    introv. specializes TautP m I.
-    specializes lift_valid m I.
-  - specializes lift_valid r1.
-    rewrite <- H3 in lift_valid.
-    destruct (rg_rule r1) eqn:E.
-    pose proof rg_wf r1. rewrite E in H.
-    rewrite <- H3 in H. inverts H.
-    unfolds in lift_valid.
-    introv. specializes TautQ m I.
-    specializes lift_valid m I.
-    simpls. contradict TautQ.
-    now apply lift_valid.
-Qed.
-
-Lemma form_2_while nd P Q :
-  rg_conc nd = form_2 P Q ->
-  rg_rule nd = Rule RGP.HL_WhileTrue ->
-  (|= P)%V ->
-  (|= ~Q)%V ->
-  exists nd' P' Q',
-  (|= P')%V /\ (|= ~Q')%V /\
-  rg_conc nd' = form_3 P' Q' /\
-  List.In nd' (rg_prems nd).
-Proof.
-  intros F Rule HP HQ.
-  pose proof rg_wf nd. rewrite Rule in H.
-  inverts H. sort.
-  destruct (rg_prems nd). discriminate.
-  simpls. inverts H0.
-  exists r (P0/\b)%A Q0.
-  rewrite F in H1. inverts H1.
-Qed.
-
-Lemma form_3_seq nd P Q :
-  rg_conc nd = form_3 P Q ->
-  rg_rule nd = Rule RGP.HL_Seq ->
-  (|= P)%V ->
-  (|= ~Q)%V ->
-  exists nd' P' Q',
-  (|= P')%V /\ (|= ~Q')%V /\
-  rg_conc nd' = form_1 P' Q' /\
-  List.In nd' (rg_prems nd).
-Proof.
-  intros F Rule HP HQ.
-  pose proof rg_wf nd. rewrite Rule in H.
-  inverts H. sort.
-  destruct (rg_prems nd). discriminate.
-  simpls. inverts H0.
-  destruct l; inverts H3. clear H4. sort.
-  rewrite F in H1. inverts H1.
-  rename Q0 into R.
-  exists r0 R Q. splits~.
-  2: { right. constructor~. }
-
-
-Definition has_form nd P Q :=
-  let conc := rg_conc nd in
-  conc = form_1 P Q \/ 
-  conc = form_2 P Q \/
-  conc = form_3 P Q.
-
-Lemma form_step nd P Q :
-  has_form nd P Q ->
-  (|= P)%V ->
-  (|= ~Q)%V ->
-  exists nd' P' Q',
-  (|= P')%V /\ (|= ~Q')%V /\
-  has_form nd' P' Q' /\
-  List.In nd' (rg_prems nd).
-Proof.
-  intros F HP HQ. destruct F as [F|F].
-  - pose proof form_1_rules _ F HP.
-    destruct H.
-    + pose proof strong_csq nd F H HP HQ. exists* H0.
-      exists nd' P' Q'. splits~. unfolds. left~.
-    + pose proof form_1_while nd F H HP HQ. exists* H0.
-      exists nd' P' Q'. splits~. right~.
-  - pose proof form_3_rules _ F.
-    destruct H.
-    + pose proof strong_csq nd F H HP HQ. exists* H0.
-      exists nd' P' Q'. splits~. unfolds. right~.
-    + pose proof form_3_seq nd F H HP HQ. exists* H0.
-      exists nd' P' Q'. splits~. left~.
-Qed.
-  
-
-Lemma path_grow nd P Q :
-  has_form nd P Q ->
-  (|= P)%V -> (|= ~Q)%V ->
-  forall n,
-  exists nd' P' Q', exists (p:path rg),
-  has_form nd' P' Q' /\
-  (|= P')%V /\ (|= ~Q')%V /\
-  first (proj1_sig p) = Some nd /\
-  last (proj1_sig p) = Some nd' /\
-  length (proj1_sig p) = S n.
-Proof.
-  intros F HP HQ.
-  induction n.
-  { intros. now exists nd P Q (single_path nd). }
-  destruct IHn as (nd'&P'&Q'&p&F'&HP'&HQ'&Fst&Lst&L). sort.
-  pose proof form_step F' HP' HQ' as (nd''&P''&Q''&HP''&HQ''&F''&In).
-  sort.
-  exists nd'' P'' Q''.
-  assert (Path : is_path (nd'::nd''::[])) by easy.
-  pose proof path_appending p (exist _ _ Path) Lst as Path'.
-  simpls. exists (exist _ _ Path').
-  simpls. splits~.
-  - rewrite~ first_append. intro. rewrite H in *. discriminate.
-  - rewrite~ last_append. easy.
-  - rewrite length_app. rewrite length_cons, length_nil, L. math.
-Qed.
-
-End GraphFacts.*)
